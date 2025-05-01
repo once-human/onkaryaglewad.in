f8b5ffba8784b7f4c366dc459b2c7f5022246997
once-human <20220802065@dypiu.ac.in>
2025-05-01 10:26:55 +0530

    feat: Integrate Microsoft Clarity analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -123,6 +123,16 @@ export default function RootLayout({
           data-goatcounter="https://oncehuman.goatcounter.com/count"
           src="//gc.zgo.at/count.js"
         />
+        {/* Microsoft Clarity Script */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rcl8ootfam");
+          `}
+        </Script>
       </body>
     </html>
   );
